48ed1aab5fd2edb812a8432e3976c69609a77d4d
osdakira <osd.akira@gmail.com>
2021-11-22 12:16:32 +0900

    feat:  bundle gem geohash_helper -t rspec

---
+++ lib/geohash_helper.rb
@@ -0,0 +1,6 @@
+require "geohash_helper/version"
+
+module GeohashHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/geohash_helper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GeohashHelper do
+  it "has a version number" do
+    expect(GeohashHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
