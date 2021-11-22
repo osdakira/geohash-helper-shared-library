f07ec26c8c74662db52c704868b0cd3823d0e03d
osdakira <osd.akira@gmail.com>
2021-11-22 13:38:25 +0900

    test: update test

--- spec/geohash_helper_spec.rb
+++ spec/geohash_helper_spec.rb
@@ -13,11 +13,31 @@ RSpec.describe GeohashHelper do
 
   describe ".intersect_geohashes" do
     it 'returns intersected geohashes' do
-      geohashes_a = %w[0 02 0123 0124 012345]
-      geohashes_b = %w[01 11]
+      geohashes_a = %w[0 10 200 201 3001 3010 3100]
 
-      got = GeohashHelper.intersect_geohashes(geohashes_a, geohashes_b)
-      expect(got.sort).to eq %w[01 0123 012345 0124]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[1]).sort).to eq %w[10]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[2]).sort).to eq %w[200 201]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[20]).sort).to eq %w[200 201]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[202]).sort).to eq %w[]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[3]).sort).to eq %w[3001 3010 3100]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[4]).sort).to eq %w[]
+      expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[1 2]).sort).to eq %w[10 200 201]
+    end
+
+    context 'when the geohashes overlap by themselves' do
+      it 'overlap is not removed' do
+        # strictly expected = %w[101 102]
+        geohashes_a = %w[10 101 102]
+        expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[10]).sort).to eq %w[10 101 102]
+
+        # 012 has 01, but 01 has 0. 01 is not removed
+        geohashes_a = %w[0 012]
+        expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[01]).sort).to eq %w[01 012]
+
+        # overlap is removed
+        geohashes_a = %w[012]
+        expect(GeohashHelper.intersect_geohashes(geohashes_a, %w[01]).sort).to eq %w[012]
+      end
     end
   end
 end
