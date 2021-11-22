6ed4d59234f702672e4c6341e421d18fe5256a8f
osdakira <osd.akira@gmail.com>
2021-11-22 12:48:22 +0900

    feat: add GeohashHelper as a module that use shared-library

--- spec/geohash_helper_spec.rb
+++ spec/geohash_helper_spec.rb
@@ -1,9 +1,23 @@
+# fronzen_string_literal: true
+
 RSpec.describe GeohashHelper do
-  it "has a version number" do
-    expect(GeohashHelper::VERSION).not_to be nil
+  describe ".intersect?" do
+    it 'can check intersect for two geohashes' do
+      expect(GeohashHelper.intersect?('012', '01')).to eq true
+      expect(GeohashHelper.intersect?('012', '012')).to eq true
+      expect(GeohashHelper.intersect?('012', '0123')).to eq true
+      expect(GeohashHelper.intersect?('012', '01234')).to eq true
+      expect(GeohashHelper.intersect?('012', '123')).to eq false
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".intersect_geohashes" do
+    it 'returns intersected geohashes' do
+      geohashes_a = %w[0 02 0123 0124 012345]
+      geohashes_b = %w[01 11]
+
+      got = GeohashHelper.intersect_geohashes(geohashes_a, geohashes_b)
+      expect(got.sort).to eq %w[01 0123 012345 0124]
+    end
   end
 end
